d19737e8a0c422c4f541fbe14d3e4c6b510447d4
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2022-07-23 15:00:16 +1000

    Try a few fizzbuzz variants. Shortest so far is 98 characters

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -7,7 +7,7 @@ process.stdin.on("data", function (input) {
   );
 });
 
-function main() {
+function mainStart() {
   for (i = 1; i <= 100; i++) {
     if (i % 15 === 0) {
       console.log("FizzBuzz");
@@ -18,5 +18,67 @@ function main() {
     } else {
       console.log(i);
     }
+  }
+}
+
+// function main() {
+//   Array(100)
+//     .fill()
+//     .forEach((_, i) => {
+//         a="fizz";b="buzz";
+//      v = m => (i+1) % m;
+//       console.log(
+//         !v(15)
+//           ? a+b
+//           : !v(3)
+//           ? a
+//           : !v(5)
+//           ? b
+//           : i+1
+//       );
+//     });
+// }
+
+//126
+// function main() {
+//   Array(100)
+//     .fill()
+//     .forEach((_, i) =>
+//       console.log(
+//         !((i + 1) % 15)
+//           ? "fizzbuzz"
+//           : !((i + 1) % 3)
+//           ? "fizz"
+//           : !((i + 1) % 5)
+//           ? "buzz"
+//           : i + 1
+//       )
+//     );
+// }
+
+// 124
+// function main() {
+//   Array(100)
+//     .fill()
+//     .map((_,i) => 1 + i)
+//     .forEach(a =>
+//       console.log(
+//         !(a % 15)
+//           ? "fizzbuzz"
+//           : !(a % 3)
+//           ? "fizz"
+//           : !(a % 5)
+//           ? "buzz"
+//           : a
+//       )
+//     );
+// }
+
+//
+function main() {
+  for (a = 1; a <= 100; a++) {
+    console.log(
+      !(a % 15) ? "fizzbuzz" : !(a % 3) ? "fizz" : !(a % 5) ? "buzz" : a
+    );
   }
 }
