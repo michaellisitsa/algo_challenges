acd7b26f415a979d09e57cb5e911dff5dd40a3f1
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-01-22 16:51:03 +1100

    Add black formatting

--- the_power_sum/test_the_power_sum.py
+++ the_power_sum/test_the_power_sum.py
@@ -1,12 +1,12 @@
-
 from the_power_sum.the_power_sum import powerSum
+
 
 class TestPowerSum:
     def test_nth_root_is_integer_returns_gt_1(self):
         # At least one solution has been found
         # sqrt(100) == 10
-       assert powerSum(100,2) > 0
+        assert powerSum(100, 2) > 0
 
     def test_nth_root_2nd_level_is_integer_returns_gt_1(self):
         # 2^2 + 3^2 == 13
-        assert powerSum(13,2) > 0
+        assert powerSum(13, 2) > 0
--- the_power_sum/the_power_sum.py
+++ the_power_sum/the_power_sum.py
@@ -7,9 +7,10 @@
 #  2. INTEGER N
 #
 
+
 def powerSum(X: int, N: int):
     # Get Nth Root of X e.g. X**(1/N)
-    nthRoot: float = X**(1/N)
+    nthRoot: float = X ** (1 / N)
     # print(nthRoot.is_integer())
     if nthRoot.is_integer():
         print("success", nthRoot)
@@ -17,9 +18,9 @@ def powerSum(X: int, N: int):
     else:
         # Floor the nthRoot
         nthRootFloor: int = int(nthRoot)
-        XLVL1: int = X - (nthRootFloor-1)**N
+        XLVL1: int = X - (nthRootFloor - 1) ** N
         print(f"XLVL1: {XLVL1}")
-        nthRootLVL1: float = XLVL1**(1/N)
+        nthRootLVL1: float = XLVL1 ** (1 / N)
         if nthRootLVL1.is_integer():
             print("success lvl1", nthRootLVL1)
             return 1
@@ -28,4 +29,4 @@ def powerSum(X: int, N: int):
             return 0
 
 
-powerSum(13,2)
+powerSum(13, 2)
