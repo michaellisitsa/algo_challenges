38e21600d2c655e2cc81ce6d6a46c0fb796a5786
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-01-23 16:25:04 +1100

    Remove unnecessary commented code

--- the_power_sum/the_power_sum.py
+++ the_power_sum/the_power_sum.py
@@ -9,38 +9,18 @@
 
 
 def powerSum(X: int, N: int, maxNum: int | None = None) -> int:
-    # Get Nth Root of X e.g. X**(1/N)
+    # Get Nth Root of X
     nthRoot: float = X ** (1 / N)
-    # if int(nthRoot) > 1, we should kick off calculations for each of the smaller numbers.
-    # return powerSum(X=result(nthRoot - 1),N)
 
     i: int = min(int(nthRoot), maxNum) if maxNum is not None else int(nthRoot)
     sum: int = 0
-    # if nthRoot.is_integer():
-    #     # print("success", nthRoot)
-    #     # print("new X: ", X - (int(nthRoot) - 1) ** N)
-    #     sum += 1
     while i > 0:
         if i == int(nthRoot) and nthRoot.is_integer():
-            # print("success", nthRoot)
-            # print("new X: ", X - (int(nthRoot) - 1) ** N)
             sum += 1
         elif i != 1:
-            # print(nthRoot.is_integer())
-            # Floor the nthRoot
-            XLVL1: int = X - i**N
-            rootXLVL1 = XLVL1 ** (1 / N)
-            sum += powerSum(XLVL1, N, maxNum=i - 1)
+            sum += powerSum(X - i**N, N, maxNum=i - 1)
         i -= 1
     return sum
 
 
 print(powerSum(100, 2))
-
-
-# def sum(a: int, b: int):
-#     return a + b
-
-
-# def sumArr(arr: list[int]) -> int:
-#     return sum(sumArr())
