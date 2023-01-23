22c2a5ce0839f0b3fca340c83b439751d198a83b
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-01-23 16:43:31 +1100

    Add relevant comments to power sum

--- the_power_sum/the_power_sum.py
+++ the_power_sum/the_power_sum.py
@@ -5,19 +5,36 @@
 # The function accepts following parameters:
 #  1. INTEGER X
 #  2. INTEGER N
-#
+#  3. maxNum: Largest unique integer that can be used.
 
 
 def powerSum(X: int, N: int, maxNum: int | None = None) -> int:
     # Get Nth Root of X
     nthRoot: float = X ** (1 / N)
 
+    # ensure only powers of unique numbers
+    # numbers above maxNum have been used already.
+    # even if the nthRoot is larger than the maxNum
     i: int = min(int(nthRoot), maxNum) if maxNum is not None else int(nthRoot)
+
     sum: int = 0
+
+    # The while loop necessary to check each value less than the nthRoot / maxNum. e.g.
+    # - 100 - 10^2
+    # - 100 - 9^2
+    # - 100 - 8^2
+
     while i > 0:
+        # Only the first iteration could lead to power sum.
+        # i.e. when nthRoot is an integer, then by definition X - i**N == 0
+        # however, if we're on subsequent iterations, i < nthRoot
+        # so X - i**N > 0.
         if i == int(nthRoot) and nthRoot.is_integer():
             sum += 1
         elif i != 1:
+            # If i is down to 1, then X - 1^2 > 0,
+            # else the above condition would've triggered
+            # as nthRoot & X would've had to be 1.
             sum += powerSum(X - i**N, N, maxNum=i - 1)
         i -= 1
     return sum
