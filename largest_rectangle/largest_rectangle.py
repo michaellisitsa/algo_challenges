cf71637f62951d90aeb1caa10266e27f6bb873ab
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-05-23 23:07:24 +1000

    Include loop for summing and removing when building gets shorter

--- largest_rectangle/largest_rectangle.py
+++ largest_rectangle/largest_rectangle.py
@@ -6,18 +6,43 @@
 
 
 def largestRectangle(h: list[int]):
-    # current_max = 0
-    current_start_indexes: list[int] = []
+    max_area: int = 0
+    start_indices: list[int] = []
     for current_height_idx, current_height in enumerate(h):
-        current_start_indexes.extend(
+        start_indices.extend(
             ascending_array_of_value(
-                previous_idx=len(current_start_indexes),
+                previous_idx=len(start_indices),
                 new_idx=current_height,
                 value=current_height_idx,
             )
         )
+        if current_height < len(start_indices):
+            # The next building is shorter, so need to sum up all the taller levels.
+            print("Index: ", current_height_idx)
+            print("current Height", current_height)
         
         print(current_start_indexes)
+            for height in reversed(range(current_height + 1, len(start_indices) + 1)):
+                area = height * (current_height_idx - start_indices[height - 1])
+                print(
+                    "For storey",
+                    height,
+                    "of width ",
+                    (current_height_idx - start_indices[height - 1]),
+                    "the area is: ",
+                    area,
+                )
+                max_area = max(area, max_area)
+                # We no longer need to track that height as we've saved it if
+                # was the maximum.
+                start_indices.pop()
+    # For the last iteration, we get the sums of the remaining values:
+    for height in range(1, len(start_indices) + 1):
+        end_position = len(h)
+        area = height * (end_position - start_indices[height - 1])
+        max_area = max(area, max_area)
+
+    return max_area
 
 
 # Shouldn't need to know about modifying the outside world
