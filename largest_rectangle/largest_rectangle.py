cf94d8397410115f2479afb58e4230d31ca787e8
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-05-23 23:15:16 +1000

    Clean up comments

--- largest_rectangle/largest_rectangle.py
+++ largest_rectangle/largest_rectangle.py
@@ -1,11 +1,8 @@
-# Complete the 'largestRectangle' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts INTEGER_ARRAY h as parameter.
-#
-
-
 def largestRectangle(h: list[int]):
+    """
+    The function is expected to return a LONG_INTEGER.
+    The function accepts INTEGER_ARRAY h as parameter.
+    """
     max_area: int = 0
     start_indices: list[int] = []
     for current_height_idx, current_height in enumerate(h):
@@ -17,24 +14,11 @@ def largestRectangle(h: list[int]):
             )
         )
         if current_height < len(start_indices):
-            # The next building is shorter, so need to sum up all the taller levels.
-            print("Index: ", current_height_idx)
-            print("current Height", current_height)
-        
-        print(current_start_indexes)
+            # The next building is shorter, so need to sum up all the taller levels, and see if they are larger than the maximum
             for height in reversed(range(current_height + 1, len(start_indices) + 1)):
                 area = height * (current_height_idx - start_indices[height - 1])
-                print(
-                    "For storey",
-                    height,
-                    "of width ",
-                    (current_height_idx - start_indices[height - 1]),
-                    "the area is: ",
-                    area,
-                )
                 max_area = max(area, max_area)
-                # We no longer need to track that height as we've saved it if
-                # was the maximum.
+                # We no longer need to track that height as we've saved it if was the maximum.
                 start_indices.pop()
     # For the last iteration, we get the sums of the remaining values:
     for height in range(1, len(start_indices) + 1):
@@ -45,10 +29,13 @@ def largestRectangle(h: list[int]):
     return max_area
 
 
-# Shouldn't need to know about modifying the outside world
 def ascending_array_of_value(previous_idx: int, new_idx: int, value: int) -> list[int]:
+    """Get an array of values if the new_idx is greater than the previous."""
     if new_idx > previous_idx:
         return [value] * (new_idx - previous_idx)
     return []
 
 
+h_array = [10, 12, 5]
+largest_area = largestRectangle(h=h_array)
+print(largest_area)
