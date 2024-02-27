493a06a8f65bb5195856f94ad9930cbf134a72ab
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-02-27 21:05:42 +1100

    Add rspec and set up first test

---
+++ AOC_2015/06_probably_a_fire_hazard/probably_a_fire_hazard.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# My documentation
+class ProbablyAFireHazard
+  def start
+    'hello'
+  end
+
+  def end
+    'bye'
+  end
+end
--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
-gem "gvl-tracing", "~> 1.4"
+gem 'gvl-tracing', '~> 1.4'
+gem 'rspec', '~> 3.13'
 gem 'rubocop', require: false
