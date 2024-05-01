810dcc5c63a214247c1de0a9f232e7e7d3f665b5
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-05-01 11:52:24 +1000

    Add empty function declaration in c++

---
+++ AOC_2015/10_look_and_say/10_look_and_say.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Write a function that returns a 2 digit array
+
+// Declaring array to a function call
+// https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
+// Instead return a string
+void next_look_and_say()
+{
+    // do nothing
+    cout << "I just got executed!" << endl;
+}
+
+int main()
+{
+    next_look_and_say(); // call the function
+    return 0;
+}
