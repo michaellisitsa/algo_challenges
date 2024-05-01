06194c1a8f649f1ee3d3af3519766e3c05aee7d4
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-05-01 12:19:26 +1000

    Add pop function for end of string

--- AOC_2015/10_look_and_say/10_look_and_say.cpp
+++ AOC_2015/10_look_and_say/10_look_and_say.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
 
-// Write a function that returns a 2 digit array
+char pop(string& myString)
+{
+    // Checking if the string is not empty before removing the last character
+    if (!myString.empty())
+    {
+        char lastValue = myString.back();
+        cout << myString << endl;
+        myString.pop_back();
+        cout << myString << endl;
+        return lastValue;
+    }
+    return '0';
+}
 
 // Declaring array to a function call
 // https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
 // Instead return a string
 string next_look_and_say(int input)
 {
-    // do nothing
     cout << "I just got executed!" << endl;
+    // 1. Convert the input to a string
+    string inputString = to_string(input);
+    // 2. Loop through the string
+    while (!inputString.empty())
+    {
+        pop(inputString);
+    }
     return to_string(input);
 }
 
