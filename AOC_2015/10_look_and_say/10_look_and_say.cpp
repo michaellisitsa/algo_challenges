01c6a16c339fb60d268163f27487abb9c216d81a
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-05-01 11:58:59 +1000

    Function parameter and return type declaration in c++

--- AOC_2015/10_look_and_say/10_look_and_say.cpp
+++ AOC_2015/10_look_and_say/10_look_and_say.cpp
@@ -6,14 +6,15 @@ using namespace std;
 // Declaring array to a function call
 // https://www.digitalocean.com/community/tutorials/return-array-in-c-plus-plus-function
 // Instead return a string
-void next_look_and_say()
+string next_look_and_say(int input)
 {
     // do nothing
     cout << "I just got executed!" << endl;
+    return to_string(input);
 }
 
 int main()
 {
-    next_look_and_say(); // call the function
+    cout << next_look_and_say(12) << endl; // call the function
     return 0;
 }
