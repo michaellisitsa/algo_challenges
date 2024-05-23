36734b6952590ea70461e873ec19176602f65764
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-05-23 11:45:34 +1000

    Finish getNeighbourCount func for non-top row

--- AOC_2015/18_gif/script.test.js
+++ AOC_2015/18_gif/script.test.js
@@ -1,21 +1,43 @@
 import getNeighbourCount from "./utils/getNeighbourCount";
 
-test("top left corner all on", () => {
+describe("all toggled", () => {
   const data = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-  expect(getNeighbourCount(0, data)).toBe(3);
+  test("top left corner", () => {
+    expect(getNeighbourCount(0, data)).toBe(3);
+  });
+
+  test("bottom left corner", () => {
+    expect(getNeighbourCount(6, data)).toBe(3);
+  });
+
+  test("bottom middle corner", () => {
+    expect(getNeighbourCount(7, data)).toBe(5);
+  });
+
+  test("bottom right corner", () => {
+    expect(getNeighbourCount(8, data)).toBe(3);
+  });
+
+  test("center", () => {
+    expect(getNeighbourCount(4, data)).toBe(8);
+  });
+
+  test("middle row right", () => {
+    expect(getNeighbourCount(5, data)).toBe(5);
+  });
 });
 
-test("top left corner all top row on", () => {
+describe("top row toggled", () => {
   const data = [1, 1, 1, 0, 0, 0, 0, 0, 0];
-  expect(getNeighbourCount(0, data)).toBe(1);
-});
+  test("top left corner", () => {
+    expect(getNeighbourCount(0, data)).toBe(1);
+  });
 
-test("middle value top row", () => {
-  const data = [1, 1, 1, 0, 0, 0, 0, 0, 0];
-  expect(getNeighbourCount(1, data)).toBe(2);
-});
+  test("middle value top row", () => {
+    expect(getNeighbourCount(1, data)).toBe(2);
+  });
 
-test("end value top row", () => {
-  const data = [1, 1, 1, 0, 0, 0, 0, 0, 0];
-  expect(getNeighbourCount(2, data)).toBe(1);
+  test("end value top row", () => {
+    expect(getNeighbourCount(2, data)).toBe(1);
+  });
 });
--- AOC_2015/18_gif/utils/getNeighbourCount.js
+++ AOC_2015/18_gif/utils/getNeighbourCount.js
@@ -1,5 +1,5 @@
-function matrixToFlatIndex(width) {
-  return (row, col) => row * width + col;
+function matrixToFlatIndex(sideLength) {
+  return (row, col) => row * sideLength + col;
 }
 
 export default function getNeighbourCount(currentIndex, data) {
@@ -10,14 +10,14 @@ export default function getNeighbourCount(currentIndex, data) {
     6 7 8
     */
   // First we need to calculate the indices of the surrounding positions
-  const width = Math.sqrt(data.length);
-  if (!Number.isInteger(width)) {
+  const sideLength = Math.sqrt(data.length);
+  if (!Number.isInteger(sideLength)) {
     throw new Error("Data is not square");
   }
-  const getIdx = matrixToFlatIndex(width);
+  const getIdx = matrixToFlatIndex(sideLength);
 
-  const row = Math.floor(currentIndex / width);
-  const col = currentIndex % width;
+  const row = Math.floor(currentIndex / sideLength);
+  const col = currentIndex % sideLength;
 
   if (row === 0) {
     if (col == 0) {
@@ -27,7 +27,7 @@ export default function getNeighbourCount(currentIndex, data) {
         data[getIdx(row + 1, col)] +
         data[getIdx(row + 1, col + 1)]
       );
-    } else if (col == width - 1) {
+    } else if (col == sideLength - 1) {
       // Top right corner
       return (
         data[getIdx(row, col - 1)] +
@@ -37,6 +37,63 @@ export default function getNeighbourCount(currentIndex, data) {
     } else {
       // Top row
       return (
+        data[getIdx(row, col - 1)] +
+        data[getIdx(row, col + 1)] +
+        data[getIdx(row + 1, col - 1)] +
+        data[getIdx(row + 1, col)] +
+        data[getIdx(row + 1, col + 1)]
+      );
+    }
+  } else if (row === sideLength - 1) {
+    if (col == 0) {
+      // Bottom left corner
+      return (
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row - 1, col + 1)] +
+        data[getIdx(row, col + 1)]
+      );
+    } else if (col == sideLength - 1) {
+      // Bottom right corner
+      return (
+        data[getIdx(row - 1, col - 1)] +
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row, col - 1)]
+      );
+    } else {
+      // Bottom row
+      return (
+        data[getIdx(row, col - 1)] +
+        data[getIdx(row, col + 1)] +
+        data[getIdx(row - 1, col - 1)] +
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row - 1, col + 1)]
+      );
+    }
+  } else {
+    if (col == 0) {
+      // Left column
+      return (
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row - 1, col + 1)] +
+        data[getIdx(row, col + 1)] +
+        data[getIdx(row + 1, col)] +
+        data[getIdx(row + 1, col + 1)]
+      );
+    } else if (col == sideLength - 1) {
+      // Right column
+      return (
+        data[getIdx(row - 1, col - 1)] +
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row, col - 1)] +
+        data[getIdx(row + 1, col - 1)] +
+        data[getIdx(row + 1, col)]
+      );
+    } else {
+      // Middle
+      return (
+        data[getIdx(row - 1, col - 1)] +
+        data[getIdx(row - 1, col)] +
+        data[getIdx(row - 1, col + 1)] +
         data[getIdx(row, col - 1)] +
         data[getIdx(row, col + 1)] +
         data[getIdx(row + 1, col - 1)] +
