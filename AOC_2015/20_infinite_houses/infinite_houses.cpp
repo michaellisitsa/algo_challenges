f201694bc813d2573daefaba60e4ecbdc211aa33
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-06-14 23:06:54 +1000

    Solve part 1 of infinite houses AOC 2015

--- AOC_2015/20_infinite_houses/infinite_houses.cpp
+++ AOC_2015/20_infinite_houses/infinite_houses.cpp
@@ -12,18 +12,15 @@ namespace infinite_houses
         }
         if (number % 2 != 0)
         {
-            std::cout << "Checking if " << number << " is prime." << std::endl;
             bool isPrime = true;
-            for (int i = 3; i <= number / 2; i += 2)
+            for (int i = 3; i * i <= number; i += 2)
             {
                 if (number % i == 0)
                 {
-                    // not a prime, do not keep looping
                     isPrime = false;
                     break;
                 }
             }
-            std::cout << number << " is prime: " << isPrime << std::endl;
             return isPrime;
         }
         else
@@ -32,29 +29,71 @@ namespace infinite_houses
         }
     }
 
-    int get_presents(int number)
+    int get_presents(int number, std::vector<int> primes)
     {
-        // Create a list of prime numbers and their current factor's cumulative total
-        // while we haven't found house with 29000000 presents
-        std::vector<int> primes;
-
-        bool isPrime = is_prime(number);
-        if (isPrime)
-        {
-            primes.push_back(number);
-        }
-
-        // Print contents of primes vector
+        std::set<int> visited_elves;
+        // Special case for end elves
+        visited_elves.insert(1);
+        visited_elves.insert(number);
         for (int prime : primes)
         {
-            std::cout << prime << " ";
+            if (prime > number / 2)
+            {
+                break;
+            }
+            if (number % prime == 0)
+            {
+                for (int multiple = prime; multiple <= number / 2; multiple += prime)
+                {
+                    if (number % multiple == 0)
+                    {
+                        visited_elves.insert(multiple);
+                    }
+                }
+            }
         }
-        return 0;
-    }
 
+        int sum = 0;
+        for (int elf : visited_elves)
+        {
+            sum += 10 * elf;
+        }
+        return sum;
+    }
 }
 int main()
 {
-    std::cout << infinite_houses::get_presents(3) << std::endl;
+    std::vector<int> primes;
+    unsigned long long number = 29000000;
+    ;
+    std::cout << "Started calculating primes" << std::endl;
+    for (int i = 2; i <= number / 2; i++)
+    {
+        if (i % 100000 == 0)
+        {
+            std::cout << "Calculating prime: " << i << std::endl;
+        }
+        if (infinite_houses::is_prime(i))
+        {
+            primes.push_back(i);
+        }
+    }
+    std::cout << "Finished calculating primes" << std::endl;
+
+    // Modify trial value to get reasonable time to completion
+    for (unsigned long long trial = 1; trial < number; trial++)
+    {
+        if (trial % 50 == 0)
+        {
+            std::cout << "Calculating presents for: " << trial << std::endl;
+        }
+        if (infinite_houses::get_presents(trial, primes) >= number)
+        {
+            std::cout << "RESULTS: " << std::endl;
+            std::cout << "House Number: " << trial << ", Presents: " << infinite_houses::get_presents(trial, primes) << std::endl;
+            std::cout << trial << std::endl;
+            break;
+        }
+    }
     return 0;
 }
