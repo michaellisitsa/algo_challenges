1cdace7e5e2cd8b81c4ea79a129a4ebc09f84025
Michael Lisitsa <michael@lisitsa.dev>
2025-05-04 14:01:56 +1000

    Extract day02 to separate func to try recursive part 2

--- AOC_2024/02.py
+++ AOC_2024/02.py
@@ -1,40 +1,47 @@
 from pathlib import Path
 
 
+def check(line):
+    vals = line.split()
+    idx = 0
+    direction = 1
+    valid = True
+    while idx < len(vals):
+        previous = int(vals[idx - 1])
+        current = int(vals[idx])
+        # Can only compare on the 2nd iteration
+        if idx == 0:
+            idx += 1
+            continue
+
+        if idx == 1:
+            # Determine direction based on the first comparison
+            if current - previous == 0:
+                valid = False
+                break
+            direction = 1 if current - previous > 0 else -1
+        difference = (current - previous) * direction
+
+        if difference > 3 or difference <= 0:
+            # Should I call recursively? so that the logic works without the next entry?
+            # Check skip_previous matches
+
+            # Early return as the sequence is invalid
+            valid = False
+            break
+        idx += 1
+    # We have reached the end and have not marked the sequence as invalid.
+    if valid:
+        return 1
+    else:
+        return 0
+
+
 def pt_1():
     with open(f"{Path(__file__).parent}/data/02.txt", "r") as f:
         sum = 0
         for line in f:
-            vals = line.split()
-            idx = 0
-            direction = 1
-            valid = True
-            while idx < len(vals):
-                previous = int(vals[idx - 1])
-                current = int(vals[idx])
-                # Can only compare on the 2nd iteration
-                if idx == 0:
-                    idx += 1
-                    continue
-
-                if idx == 1:
-                    # Determine direction based on the first comparison
-                    if current - previous == 0:
-                        valid = False
-                        break
-                    direction = 1 if current - previous > 0 else -1
-
-                difference = (current - previous) * direction
-
-                if difference > 3 or difference <= 0:
-                    # Early return as the sequence is invalid
-                    valid = False
-                    break
-                idx += 1
-            # We have reached the end and have not marked the sequence as invalid.
-            if valid:
-                sum += 1
-
+            sum += check(line)
     return sum
 
 
