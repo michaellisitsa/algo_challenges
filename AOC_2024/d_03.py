a48b2c3bad7b882622d429eb2621f0f3a852089f
Michael Lisitsa <michael@lisitsa.dev>
2025-08-30 15:56:38 +1000

    Finish part 1 of aoc24-3

--- AOC_2024/d_03.py
+++ AOC_2024/d_03.py
@@ -5,15 +5,23 @@ import re
 def pt1():
     with open(f"{Path(__file__).parent}/data/03.txt", "r") as f:
         result = []
+        total = 0
         for line in f:
-            get_sum_expr(line)
+            pairs = get_mul_expr(line)
+            for pair in pairs:
+                total += mul_tuple(pair)
+        return total
 
 
 def get_mul_expr(string: str):
-    m = re.findall(r"mul\(\d+,\d+\)", string)
-    print(f"{string=}")
+    m = re.findall(r"mul\((\d+),(\d+)\)", string)
     return m
 
 
+def mul_tuple(values: tuple[str, str]):
+    a, b = values
+    return int(a) * int(b)
+
+
 if __name__ == "__main__":
-    pt1()
+    print(pt1())
--- AOC_2024/test_03.py
+++ AOC_2024/test_03.py
@@ -1,9 +1,14 @@
-from .d_03 import get_mul_expr
+from .d_03 import get_mul_expr, mul_tuple
 
 
 def test_get_mul_expression_whole_word():
-    assert get_mul_expr("mul(1,2)") == ["mul(1,2)"]
+    assert get_mul_expr("mul(1,2)") == [("1", "2")]
 
 
 def test_get_mul_expression_partial():
-    assert get_mul_expr("my_mul(1,2), my_mul(3,4)") == ["mul(1,2)", "mul(3,4)"]
+    assert get_mul_expr("my_mul(1,2), my_mul(3,4)") == [("1", "2"), ("3", "4")]
+
+
+def test_mul_tuple():
+    assert mul_tuple(("1", "2")) == 2
+    assert mul_tuple(("3", "4")) == 12
