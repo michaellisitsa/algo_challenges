97e820927fc444aca66576acd499cb5b8a09e28e
Michael Lisitsa <michael@lisitsa.dev>
2025-12-02 09:17:06 +1100

    code cleanup

--- AOC_2025/d01/main.py
+++ AOC_2025/d01/main.py
@@ -1,32 +1,27 @@
 from pathlib import Path
-import re
 
 
 def day01():
     with open(f"{Path(__file__).parent}/data/01.txt", "r") as f:
         dial = 50
         password = 0
-        password2 = 0
-        for idx, line in enumerate(f):
+        for line in f:
             direction = line[0]
             count = int(line[1:-1])
             count = count if direction == "R" else -count
             sum = dial + count
-            remainder = abs(sum) % 100
-            rotations = (abs(sum) - remainder) // 100
+            rotations, remainder = divmod(abs(sum), 100)
             if sum > 0:
-                # if count > 99:
                 # 0 -> 0 (rotations 0) = minus 0
                 # 150 -> 50 (1 rotation) = minus 100
                 # 100 -> 0 (1 rotation) = minus 100
                 # 250 -> 50 (2 rotations) = minus 200
                 dial = sum - (rotations * 100)
             elif sum < 0:
-                # if count < 0:
-                # -20 -> 80 (0 rotations) = plus 100
-                # -99 -> 1 (0 rotations) = plus 100
+                # -20 -> 80 (1 rotations) = plus 100
+                # -99 -> 1 (1 rotations) = plus 100
                 # -100 -> 0 (1 rotations) = plus 100
-                # -101 -> 99 (1 rotations) = plus 200
+                # -101 -> 99 (2 rotations) = plus 200
                 # -230 -> 70 (3 rotations) = plus 300
                 rotations = rotations if remainder == 0 else rotations + 1
                 dial = sum + (rotations * 100)
