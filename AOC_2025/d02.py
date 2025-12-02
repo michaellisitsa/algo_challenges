0d186403c5065658350ec37d800cd7b65a9f913a
Michael Lisitsa <michael@lisitsa.dev>
2025-12-03 09:01:35 +1100

    day 2 pt 2 aoc 2025

--- AOC_2025/d02.py
+++ AOC_2025/d02.py
@@ -2,6 +2,17 @@ from pathlib import Path
 import typing as t
 
 
+def divisorGenerator(n):
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            yield i
+
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
 def run():
     with open(f"{Path(__file__).parent}/data/02.txt", "r") as f:
         pt1 = 0
@@ -9,10 +20,18 @@ def run():
         ranges = f.readline().strip("\n").split(",")
         for rng in ranges:
             start, _, end = rng.partition("-")
+            print(f"{start=}")
             for id_num in range(int(start), int(end) + 1):
                 id = str(id_num)
                 if len(id) % 2 == 0 and id[: (len(id) // 2)] == id[(len(id) // 2) :]:
                     pt1 += int(id)
+                found = False
+                for divisor in divisorGenerator(len(id)):
+                    value = id[:divisor]
+                    if all([chunk == value for chunk in chunks(id, divisor)]):
+                        found = True
+                if found:
+                    pt2 += int(id)
 
         return pt1, pt2
 
