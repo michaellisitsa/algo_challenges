9a2a4265d67b35f0e3f9c74db02803a992b03667
Michael Lisitsa <michael@lisitsa.dev>
2025-12-02 21:27:15 +1100

    part 2 day 1 2025 AOC

--- AOC_2025/d01/main.py
+++ AOC_2025/d01/main.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 
 
-def day01():
+def run():
     with open(f"{Path(__file__).parent}/data/01.txt", "r") as f:
         dial = 50
-        password = 0
+        pt1 = 0
+        pt2 = 0
         for line in f:
-            direction = line[0]
             count = int(line[1:-1])
-            count = count if direction == "R" else -count
+            count = count if line[0] == "R" else -count
             sum = dial + count
             rotations, remainder = divmod(abs(sum), 100)
+            passes = rotations
+            prev_dial = dial
             if sum > 0:
                 # 0 -> 0 (rotations 0) = minus 0
                 # 150 -> 50 (1 rotation) = minus 100
                 # 100 -> 0 (1 rotation) = minus 100
                 # 250 -> 50 (2 rotations) = minus 200
+                # 50 -> 0 (0 rotations) = leave
                 dial = sum - (rotations * 100)
             elif sum < 0:
                 # -20 -> 80 (1 rotations) = plus 100
@@ -23,15 +26,29 @@ def day01():
                 # -100 -> 0 (1 rotations) = plus 100
                 # -101 -> 99 (2 rotations) = plus 200
                 # -230 -> 70 (3 rotations) = plus 300
+                # -20 -> 0 (0 rotation) = none
                 rotations = rotations if remainder == 0 else rotations + 1
                 dial = sum + (rotations * 100)
             else:
                 dial = 0
 
             if dial == 0:
-                password += 1
-        return password
+                # If landing on zero, rotations will take into account passing 0.
+                # We don't also need to count landing on zero.
+                # e.g. 45 + R55 = 100 -> dial=0
+                # (1 pass only)
+                if count < 0:
+                    pt2 += 1
+                pt1 += 1
+            if prev_dial == 0 and count < 0:
+                # This should render as only 0 rotation, but we've had to wind clock forward 100
+                # 0 + L14 = -14 -> dial=86
+                pt2 += rotations - 1
+            else:
+                pt2 += rotations
+
+        return pt1, pt2
 
 
 if __name__ == "__main__":
-    print(day01())
+    print(run())
