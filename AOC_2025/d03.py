003c215a0705d790ad4b6a06c6ac67511b68cad7
Michael Lisitsa <michael@lisitsa.dev>
2025-12-03 22:27:31 +1100

    Day 3 part 2 AOC 2025

--- AOC_2025/d03.py
+++ AOC_2025/d03.py
@@ -1,27 +1,50 @@
 from pathlib import Path
 
-def run():
+
+def pt1():
     with open(f"{Path(__file__).parent}/data/03.txt", "r") as f:
-        pt1 = 0
-        pt2 = 0
+        result = 0
         for line in f:
             line = line.strip("\n")
-            print(f"{line}")
-            print(line[:-1])
-            first_idx: int = 0
+            start_idx: int = 0
             first: int = 0
             for idx, digit in enumerate(line[:-1]):
                 if int(digit) > first:
                     first = int(digit)
-                    first_idx = idx
-            if first_idx == len(line) - 2:
-                # Slice with single value is an int so cannot be iterated
-                second = int(line[-1])
-            else:
-                second = max(*[int(digit) for digit in line[(first_idx + 1) :]])
-            pt1 += first * 10 + second
-        return pt1, pt2
+                    start_idx = idx + 1
+            result += first * 10
+            result += (
+                int(line[-1])
+                if start_idx == len(line) - 1
+                else max(*[int(digit) for digit in line[start_idx:]])
+            )
+        return result
+
+
+def pt2():
+    with open(f"{Path(__file__).parent}/data/03.txt", "r") as f:
+        result = 0
+        for line in f:
+            line = line.strip("\n")
+            start_idx: int = 0
+            slice_end = -11
+            while slice_end < 0:
+                cur_idx: int = 0
+                cur_digit = 0
+                for idx, digit in enumerate(line[start_idx:slice_end]):
+                    if int(digit) > cur_digit:
+                        cur_digit = int(digit)
+                        cur_idx = idx + start_idx
+                result += 10 ** (-slice_end) * cur_digit
+                slice_end += 1
+                start_idx = cur_idx + 1
+            result += (
+                int(line[-1])
+                if start_idx == len(line) - 1
+                else max(*[int(digit) for digit in line[start_idx:]])
+            )
+        return result
 
 
 if __name__ == "__main__":
-    print(run())
+    print(f"{pt1()=} {pt2()=}")
