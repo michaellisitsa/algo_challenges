461715df78934d20ba76d73d6660225e56a13f5f
Michael Lisitsa <michael@lisitsa.dev>
2025-12-05 17:01:41 +1100

    Pt 2 day 5 aoc 2025

--- AOC_2025/d05.py
+++ AOC_2025/d05.py
@@ -1,10 +1,10 @@
 from pathlib import Path
+import operator
 
 
-def run():
+def pt1():
     with open(f"{Path(__file__).parent}/data/05.txt", "r") as f:
-        pt1 = 0
-        pt2 = 0
+        result = 0
         ranges = []
         adding_ranges = True
         for line in f:
@@ -17,10 +17,40 @@ def run():
             else:
                 for rng in ranges:
                     if int(line.strip("\n")) <= rng[1] and int(line) >= rng[0]:
-                        pt1 += 1
+                        result += 1
                         break
-        return pt1, pt2
+        return result
+
+
+def pt2():
+    with open(f"{Path(__file__).parent}/data/05.txt", "r") as f:
+        result = 0
+        ranges = []
+        adding_ranges = True
+        for line in f:
+            if line == "\n":
+                adding_ranges = False
+                break
+            if adding_ranges is True:
+                start, _, end = line.strip("\n").partition("-")
+                ranges.append({"start": int(start), "end": int(end)})
+
+        ranges = sorted(ranges, key=operator.itemgetter("start"))
+        # print(f"{ranges=}")
+        compacted_ranges = []
+        for idx, rng in enumerate(ranges):
+            if not compacted_ranges:
+                compacted_ranges.append(ranges[0])
+            else:
+                last = compacted_ranges[-1]
+                if rng["end"] >= last["start"] and rng["start"] <= last["end"]:
+                    # Compact the last range
+                    last["end"] = max(last["end"], rng["end"])
+                else:
+                    compacted_ranges.append(rng)
+
+        return sum((rng["end"] - rng["start"]) + 1 for rng in compacted_ranges)
 
 
 if __name__ == "__main__":
-    print(f"{run()=} ")
+    print(f"{pt1()=} {pt2()=}")
