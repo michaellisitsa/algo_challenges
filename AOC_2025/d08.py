8f14898282156a0e83e80baacde465874fa6fd11
Michael Lisitsa <michael@lisitsa.dev>
2025-12-11 13:59:06 +1100

    Handle unioning 2 disjoint sets

--- AOC_2025/d08.py
+++ AOC_2025/d08.py
@@ -12,6 +12,7 @@ def pt1():
     coord_to_set = {}
     with open(f"{Path(__file__).parent}/data/08.txt", "r") as f:
         result = 0
+        # for each  pair of  points On^2  we  calculate their distance from each other in 3d space.
         for line in f:
             coords.append(
                 tuple([int(coord) for coord in line.rstrip().split(",")])
@@ -24,10 +25,22 @@ def pt1():
             # https://stackoverflow.com/questions/45137002/define-heap-key-for-an-array-of-tuples#45137076
             heapq.heappush(pq, (distance, pt1, pt2))
         i = 0
-        while i < len(pq) - 1 and i < 10 and len(coord_to_set) <= len(coords):
+        while i < len(pq) - 1 and i < 1000 and len(coord_to_set) <= len(coords):
             distance, pt1, pt2 = heapq.heappop(pq)
             if pt1 in coord_to_set and pt2 in coord_to_set:
-                continue
+                # Join 2 disjoint sets
+                if coord_to_set[pt1] != coord_to_set[pt2]:
+                    set_1 = disjoint_sets[coord_to_set[pt1]]
+                    set_2 = disjoint_sets[coord_to_set[pt2]]
+                    union_set = set_1 | set_2
+                    disjoint_sets.append(union_set)
+                    # sentinel value for deleted records so we don't have to renumber all other records
+                    disjoint_sets[coord_to_set[pt1]] = set()
+                    disjoint_sets[coord_to_set[pt2]] = set()
+                    for item in union_set:
+                        coord_to_set[item] = (
+                            len(disjoint_sets) - 1
+                        )  # Update the cache with the latest
             elif pt1 in coord_to_set:
                 set_idx = coord_to_set[pt1]
                 disjoint_sets[set_idx].add(pt2)
